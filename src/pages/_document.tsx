ab6d9aa742a48a3209bef72b737693978ac2ba3f
Leonardo Tavares <49364517+leotavares21@users.noreply.github.com>
2022-04-19 08:45:23 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,9 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MYDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="pt-br">
         <Head>
+          <meta charset="utf-8" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Anton&family=Bangers&family=Open+Sans:wght@300;400;600;700&display=swap"
